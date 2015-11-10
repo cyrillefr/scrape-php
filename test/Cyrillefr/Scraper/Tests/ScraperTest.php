1897132f9efb54517721033da3b6c13055121bed
cyrille <cyrille.vasseur@gmail.com>
2015-11-10 01:24:44 +0000

    Tests

---
+++ test/Cyrillefr/Scraper/Tests/ScraperTest.php
@@ -0,0 +1,10 @@
+<?php
+
+    namespace Cyrillefr\Scraper\Tests;
+
+    use namespace Cyrillefr\Scraper\Scraper;
+    use PHPUnit_Framework_TestCase;
+
+    class ScraperTest extends PHPUnit_Framework_TestCase {
+
+    }
