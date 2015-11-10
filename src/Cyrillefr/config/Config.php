5412dbde85cc4492824ee69c62ed4a676de62f7b
cyrille <cyrille.vasseur@gmail.com>
2015-11-10 08:56:06 +0000

    Lazy instantiation

--- src/Cyrillefr/config/Config.php
+++ src/Cyrillefr/config/Config.php
@@ -5,13 +5,19 @@
     class Config {
 
 
+        private $arr_of_keys_conf;
+
         /**
          * $conf = Config::get(key)
          *
          */
         static function get($key){
-            $arr = parse_ini_file('scrap.ini');
-            return $arr[$key];
+
+            if (! isset($this->arr_of_keys_conf))
+            {
+                $this->arr_of_keys_conf = parse_ini_file('scrap.ini');
+                return $this->arr_of_keys_conf;
+            }
         }
 
     }
